b4243e49cc5342fa924dedbfded547230d8fdf8e
hwan17 <kimmh6017@gmail.com>
2021-01-22 00:12:07 +0900

    Update my code

--- morsecode.py
+++ morsecode.py
@@ -85,7 +85,7 @@ def is_validated_english_sentence(user_input):
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당 또는 필요에 따라 자유로운 수정
     result = re.findall('\d+',user_input) == []\
-        and re.findall('[_@#$%^&*\(\)\-+=\[\]\{\}\"\';:\\|`~]',user_input) == []\
+        and re.findall('[\_\@\#\$\%\^\&\*\(\)\-\+=\[\]\{\}\"\'\;\:\\\|\`\~]',user_input) == []\
         and (len(re.sub('[.,!\?]','',user_input)) > 0 and re.sub(' ','',user_input) != '')
 
     return result
